0d0eaacc8523b2512f721ed2940060b4970c23c8
Engjell Ramadani <eramadani@wpi.edu>
2023-03-06 00:48:31 -0500

    Scrapes data correctly now

--- Main.py
+++ Main.py
@@ -18,17 +18,27 @@ try:
     # Wait for the table to load
     table_xpath = "//table[@class='Crom_table__p1iZz']"
     table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, table_xpath)))
-    
+
+    data = []
     # Extract the table data using BeautifulSoup
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    rows = soup.select('table tr')
-    
-    # Print the data
-    for row in rows:
-        cells = row.find_all('td')
-        for cell in cells:
-            print(cell.text.strip())
+    table = soup.find('table', {'class': 'Crom_table__p1iZz'})
+    rows = table.find_all('tr')
 
+    # Extract the table headers
+    header_row = rows[0]
+    headers = [th.text.strip() for th in header_row.find_all('th')]
+
+    # Extract the table data
+    for row in rows[1:]:
+        cells = row.find_all('td')
+        row_data = []
+        for cell in cells:
+            row_data.append(cell.text.strip())
+        data.append(row_data)
+
+    df = pd.DataFrame(data, columns=headers)
+    print(df)
 finally:
     # Quit the driver
     driver.quit()
