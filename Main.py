e57e4620e86ff97c60ceb9e1403fc25d981ffdc6
Engjell Ramadani <eramadani@wpi.edu>
2023-03-06 22:25:57 -0500

    Gathered data from 2010-2023

--- Main.py
+++ Main.py
@@ -6,41 +6,51 @@ from selenium.webdriver.safari.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 pd.set_option('display.max_columns', None)
-url = 'https://www.nba.com/stats/leaders?Season=2019-20&PerMode=Totals'
 
-driver = webdriver.Safari()
-# Set up Selenium webdriver
+# Loop through each season from 2010-11 to 2022-23
+for year in range(2010, 2023):
+    season = str(year) + '-' + str(year+1)[-2:]
+    print(f"Processing data for {season} season...")
 
-driver.get(url)
+    # Construct the URL for the NBA stats page for the current season
+    url = f"https://www.nba.com/stats/leaders?Season={season}&PerMode=Totals"
 
-try:
-    # Wait for the table to load
-    table_xpath = "//table[@class='Crom_table__p1iZz']"
-    table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, table_xpath)))
+    # Set up Selenium webdriver
+    driver = webdriver.Safari()
 
-    data = []
-    # Extract the table data using BeautifulSoup
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    table = soup.find('table', {'class': 'Crom_table__p1iZz'})
-    rows = table.find_all('tr')
+    try:
+        # Load the NBA stats page for the current season
+        driver.get(url)
 
-    # Extract the table headers
-    header_row = rows[0]
-    headers = [th.text.strip() for th in header_row.find_all('th')]
+        # Wait for the table to load
+        table_xpath = "//table[@class='Crom_table__p1iZz']"
+        table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, table_xpath)))
 
-    # Extract the table data
-    for row in rows[1:]:
-        cells = row.find_all('td')
-        row_data = []
-        for cell in cells:
-            row_data.append(cell.text.strip())
-        data.append(row_data)
+        data = []
+        # Extract the table data using BeautifulSoup
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        table = soup.find('table', {'class': 'Crom_table__p1iZz'})
+        rows = table.find_all('tr')
 
-    df = pd.DataFrame(data, columns=headers)
+        # Extract the table headers
+        header_row = rows[0]
+        headers = [th.text.strip() for th in header_row.find_all('th')]
 
-    print(df)
-finally:
-    # Quit the driver
-    driver.quit()
+        # Extract the table data
+        for row in rows[1:]:
+            cells = row.find_all('td')
+            row_data = []
+            for cell in cells:
+                row_data.append(cell.text.strip())
+            data.append(row_data)
+
+        df = pd.DataFrame(data, columns=headers)
+
+        # Output the data to a CSV file for the current season
+        filename = f"{season}_stats.csv"
+        df.to_csv(filename, index=False)
+
+    finally:
+        # Quit the driver
+        driver.quit()
